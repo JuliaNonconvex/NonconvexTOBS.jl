8a6445d935c85905f7b1cdfd28ab2b43bc62f76d
Mohamed Tarek <mohamed82008@gmail.com>
2022-04-06 21:13:45 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/NonconvexTOBS.jl
@@ -0,0 +1,5 @@
+module NonconvexTOBS
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using NonconvexTOBS
+using Test
+
+@testset "NonconvexTOBS.jl" begin
+    # Write your tests here.
+end
