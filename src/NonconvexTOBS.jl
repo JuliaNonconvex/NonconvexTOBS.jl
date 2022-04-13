9c727318f375616100c2a0818020dd8a4353ac67
Lucas Pereira <lucas.lmspereira@gmail.com>
2022-04-13 12:21:24 -0300

    updateMeeting

--- src/NonconvexTOBS.jl
+++ src/NonconvexTOBS.jl
@@ -1,33 +1,33 @@
 module NonconvexTOBS
 
-export TOBSalg, TOBSOptions
+export TOBSAlg, TOBSOptions
 
-using Reexport, Parameters, SparseArrays, Zygote
+using Reexport, Parameters, SparseArrays, Zygote, JuMP, Cbc
 @reexport using NonconvexCore
 using NonconvexCore: @params, VecModel, AbstractResult
 using NonconvexCore: AbstractOptimizer, CountingFunction
 using NonconvexCore: nvalues, fill_indices!, add_values!, _dot
 import NonconvexCore: optimize!
-using TOBS
 
-@with_kw struct TOBSOptions
+struct TOBSAlg <: AbstractOptimizer end
+
+struct TOBSOptions
     nt::NamedTuple # one of the fields of the nt is milp_options
-    timeLimit::Real = 1.0
-    optimizer::TOBSalg{S} # ?
-    # optimSolver = Cbc.optimizer
-    β::Real = 0.1 # move limit parameter
-    N::Int = 5 # number of past iterations for moving average calculation
-    τ::Real = 0.0001 # convergence parameter (upper bound of error)
-    ϵ::Real = 0.01 # constraint relaxation parameter
 end
 
-function TOBSOptions(; kwargs...,)
-    return TOBSOptions(kwargs)
+function TOBSOptions(
+    ;β::Real = 0.1, # move limit parameter
+    N::Int = 5, # number of past iterations for moving average calculation
+    τ::Real = 0.0001, # convergence parameter (upper bound of error)
+    ϵ::Real = 0.01, # constraint relaxation parameter
+    timeLimit::Real = 1.0,
+    optimizer = Cbc.Optimizer,
+)
+    return TOBSOptions((; β, N, τ, ϵ, timeLimit, optimizer))
 end
 
 @params mutable struct TOBSWorkspace <: Workspace
     model::VecModel
-    problem # ?
     x0::AbstractVector
     options::TOBSOptions
     counter::Base.RefValue{Int}
@@ -36,75 +36,57 @@ function TOBSWorkspace(
     model::VecModel, x0::AbstractVector = Nonconvex.getinit(model);
     options = TOBSOptions(), kwargs...,
 )
-    problem, counter = get_TOBS_problem(
-        model, copy(x0),
-        options.nt.hessian_approximation == "limited-memory",
-        options.nt.jac_c_constant == "yes" && options.nt.jac_d_constant == "yes",
-    )
-    return TOBSWorkspace(model, problem, copy(x0), options, counter)
+    return TOBSWorkspace(model, copy(x0), options, Ref(0))
 end
 @params struct TOBSResult <: AbstractResult
     minimizer
     minimum
-    problem
-    status
     fcalls::Int
 end
 
 function optimize!(workspace::TOBSWorkspace)
-    @unpack model, problem, x0, options, counter = workspace
-    problem.x .= x0
+    @unpack model, x0, options, counter = workspace
     counter[] = 0
-    foreach(keys(options.nt)) do k
-        v = options.nt[k]
-        addOption(problem, string(k), v)
-    end
-    solvestat = TOBSSolve(problem, workspace)
-    return TOBSResult(
-        copy(problem.x), NonconvexCore.getobjective(model)(problem.x),
-        problem, solvestat, counter[]
-    )
-end
-
-struct TOBSalg{S} <: AbstractOptimizer
-    milp::S
-end
-
-function TOBSSolve(workspace)
-    # milp_solver = optimizer_with_attributes(Cbc.Optimizer)
-    milp_solver = optimizer_with_attributes(workspace.options.optimizer)
-    workspace.problem.x .= workspace.x0
-    numVars = length(workspace.problem.x)
+    @unpack β, N, τ, ϵ, timeLimit, optimizer = options.nt
+    milp_solver = optimizer_with_attributes(optimizer)
+    numVars = length(NonconvexCore.getinit(model))
     count = 1 # iteration counter
-    comps = zeros(workspace.options.N) # recent history of compliance values
+    comps = zeros(N) # recent history of compliance values
+    if any(NonconvexCore.getmin(model) .!= 0) || any(NonconvexCore.getmax(model) .!= 1)
+        throw(ArgumentError("Lower bound must be 0 and upper bound must be 1."))
+    end
+    er = 1
+    # x = NonconvexCore.getinit(model)
+    x = ones(numVars)
+    currentConstr = model.ineq_constraints(x)
 
     m = JuMP.Model(milp_solver)
-    while workspace.options.τ < workspace.options.er || currentConstr > constrVal
+    while τ < er || any(currentConstr .> 0)
         count > 1 && (m = JuMP.Model(milp_solver))
         set_optimizer_attribute(m, "logLevel", 0)
-        set_optimizer_attribute(m, "seconds", workspace.options.timeLimit)
+        set_optimizer_attribute(m, "seconds", timeLimit)
 
-        sens = Zygote.gradient(NonconvexCore.getobjective(workspace.model), x)[1]
-        # gradConstr = Zygote.gradient(constr, x)[1]
-        jacConstr = Zygote.jacobian(m.ineqconstraints, x)[1]
-        # in slack: NonconvexCore.getineqconstraints(model) ?
+        sens = Zygote.gradient(NonconvexCore.getobjective(model), x)[1]
+        jacConstr = Zygote.jacobian(model.ineq_constraints, x)[1]
 
         # Define optimization subproblem variables (change in each variable of original problem for this iteration)
         @variable(m, deltaX[1:numVars], Int)
         set_lower_bound.(deltaX, -x)
         set_upper_bound.(deltaX, 1 .- x)
         # Constrain volume change per iteration
-        @constraint(m, sum(x -> x^2, deltaX) <= workspace.options.β*numVars)
+        @constraint(m, sum(deltaX) <= β*numVars)
         # Constraint relaxation
-        if constrVal < (1 - workspace.options.ϵ) * currentConstr
-            Δ = -workspace.options.ϵ * currentConstr
-        elseif constrVal > (1 + workspace.options.ϵ) * currentConstr
-            Δ = workspace.options.ϵ * currentConstr
-        else
-            Δ = constrVal - currentConstr
+        currentConstr = model.ineq_constraints(x)
+        Δ = map(currentConstr) do c
+            if 0 < (1 - ϵ) * c
+                return -ϵ * c
+            elseif 0 > (1 + ϵ) * c
+                return ϵ * c
+            else
+                return -c
+            end
         end
-        @constraint(m, jacConstr' * deltaX <= Δ)
-        # g(x) = model.ineq_constraints(x) -> g(x) <= 0
+        @constraint(m, jacConstr * deltaX .<= Δ)
         # Define optimization objective
         @objective(m, Min, sens' * deltaX)
         # Optimize linearized problem
@@ -113,23 +95,20 @@ function TOBSSolve(workspace)
         x += JuMP.value.(deltaX)
         # Store recent history of objectives
         comps[1:end-1] .= comps[2:end]
-        comps[end] = workspace.model.objective.f(x)
+        comps[end] = getobjective(model)(x)
         if count > 2 * N - 1
-            workspace.options.er = abs(sum([comps[i] - comps[i - 1] for i in 2:N])) / sum(comps)
+            er = abs(sum([comps[i] - comps[i - 1] for i in 2:N])) / sum(comps)
         end
-        # currentConstr = volfrac(filter(x))
         
-        @info "iter = $count, obj = $(round.(comps[end]; digits=3)), vol_frac = $(round(currentConstr, digits=3)), er = $(round(workspace.options.er, digits=3))"
-        count += 1        
+        @info "iter = $count, obj = $(round.(comps[end]; digits=3)), constraint violation norm = $(round(norm(currentConstr), digits=3)), er = $(round(er, digits=3))"
+        count += 1
     end
+
+    return TOBSResult(copy(x), NonconvexCore.getobjective(model)(x), counter[])
 end
 
-function Workspace(model::VecModel, optimizer::TOBSalg, x0::AbstractVector; kwargs...,)
+function Workspace(model::VecModel, optimizer::TOBSAlg, x0::AbstractVector; kwargs...,)
     return TOBSWorkspace(model, x0; kwargs...)
-end
-
-function get_TOBS_problem()
-    
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,43 @@
-using NonconvexTOBS
-using Test
+using NonconvexTOBS, TopOpt, LinearAlgebra, Test
 
 @testset "NonconvexTOBS.jl" begin
-    # Write your tests here.
+    E = 1.0 # Young’s modulus
+    v = 0.3 # Poisson’s ratio
+    f = 1.0 # downward force
+    rmin = 4.0 # filter radius
+    xmin = 0.0001 # minimum density
+    problem_size = (60, 20)
+    V = 0.5 # maximum volume fraction
+    p = 4.0 # penalty
+    x0 = fill(V, prod(problem_size)) # initial design
+
+    problem = HalfMBB(Val{:Linear}, problem_size, (1.0, 1.0), E, v, f)
+
+    solver = FEASolver(Direct, problem; xmin=xmin)
+    cheqfilter = DensityFilter(solver; rmin=rmin)
+    comp = TopOpt.Compliance(problem, solver)
+
+    function obj(x)
+        # minimize compliance
+        return comp(cheqfilter(x))
+    end
+    function constr(x)
+        # volume fraction constraint
+        return sum(cheqfilter(x)) / length(x) - V
+    end
+
+    m = Model(obj)
+    addvar!(m, zeros(length(x0)), ones(length(x0)))
+    Nonconvex.add_ineq_constraint!(m, constr)
+
+    options = TOBSOptions()
+    TopOpt.setpenalty!(solver, p)
+    # Method of Moving Asymptotes
+    @time r = Nonconvex.optimize(m, TOBSAlg(), x0; options=options)
+
+    @show obj(r.minimizer)
+    @show constr(r.minimizer)
+    topology = cheqfilter(r.minimizer)
+
+
 end
