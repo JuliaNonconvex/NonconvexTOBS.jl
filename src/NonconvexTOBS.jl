b9792b7cf6b30bec74a26e69b7c7fadbb4074d85
Lucas Pereira <lucas.lmspereira@gmail.com>
2022-04-15 16:27:52 -0300

    tests and time stabilization

--- src/NonconvexTOBS.jl
+++ src/NonconvexTOBS.jl
@@ -16,15 +16,16 @@ struct TOBSOptions
 end
 
 function TOBSOptions(
-    ;β::Real = 0.05, # move limit parameter
-    N::Int = 20, # number of past iterations for moving average calculation
-    τ::Real = 0.001, # convergence parameter (upper bound of error)
-    ϵ::Real = 0.05, # constraint relaxation parameter
+    ;movelimit::Real = 0.05, # move limit parameter
+    pastN::Int = 20, # number of past iterations for moving average calculation
+    convParam::Real = 0.001, # convergence parameter (upper bound of error)
+    constrRelax::Real = 0.05, # constraint relaxation parameter
     timeLimit::Real = 1.0,
     optimizer = Cbc.Optimizer,
-    maxiter::Int = 200
+    maxiter::Int = 200,
+    timeStable::Bool = false
 )
-    return TOBSOptions((; β, N, τ, ϵ, timeLimit, optimizer, maxiter))
+    return TOBSOptions((; movelimit, pastN, convParam, constrRelax, timeLimit, optimizer, maxiter, timeStable))
 end
 
 @params mutable struct TOBSWorkspace <: Workspace
@@ -46,11 +47,11 @@ end
 
 function optimize!(workspace::TOBSWorkspace)
     @unpack model, x0, options = workspace
-    @unpack β, N, τ, ϵ, timeLimit, optimizer, maxiter = options.nt
+    @unpack movelimit, pastN, convParam, constrRelax, timeLimit, optimizer, maxiter, timeStable = options.nt
     milp_solver = JuMP.optimizer_with_attributes(optimizer)
     numVars = length(NonconvexCore.getinit(model))
     count = 1 # iteration counter
-    comps = zeros(N) # recent history of compliance values
+    objHist = zeros(pastN) # recent history of compliance values
     if any(NonconvexCore.getmin(model) .!= 0) || any(NonconvexCore.getmax(model) .!= 1)
         throw(ArgumentError("Lower bound must be 0 and upper bound must be 1."))
     end
@@ -58,11 +59,12 @@ function optimize!(workspace::TOBSWorkspace)
     x = ones(numVars)
     currentConstr, jacConstr = NonconvexCore.value_jacobian(model.ineq_constraints, x)
     objval, objgrad = NonconvexCore.value_gradient(getobjective(model), x)
+    pastGrad = copy(objgrad)
     best_sol = (x, objval, currentConstr, norm(currentConstr))
 
     m = JuMP.Model(milp_solver)
     skip_step = false
-    while (τ < er || any(currentConstr .> 0)) && count < maxiter
+    while (convParam < er || any(currentConstr .> 0)) && count < maxiter
         count > 1 && (m = JuMP.Model(milp_solver))
         JuMP.set_optimizer_attribute(m, "logLevel", 0)
         JuMP.set_optimizer_attribute(m, "seconds", timeLimit)
@@ -85,11 +87,11 @@ function optimize!(workspace::TOBSWorkspace)
         JuMP.set_upper_bound.(absdeltaX, 1)
         JuMP.@constraint(m, deltaX .<= absdeltaX)
         JuMP.@constraint(m, .-deltaX .<= absdeltaX)
-        # Constrain volume change per iteration
-        JuMP.@constraint(m, sum(absdeltaX) <= β*numVars)
+        # Constrain amount of change per iteration
+        JuMP.@constraint(m, sum(absdeltaX) <= movelimit*numVars)
         # Constraint relaxation
         Δ = map(currentConstr) do c
-            abs(c) < ϵ ? -c : -ϵ * c
+            abs(c) < constrRelax ? -c : -constrRelax * c
         end
         JuMP.@constraint(m, jacConstr * deltaX .<= Δ)
         # Define optimization objective
@@ -110,19 +112,24 @@ function optimize!(workspace::TOBSWorkspace)
         end
         if skip_step
             @warn "Subproblem is infeasible. Temporarily relaxing the subproblem."
-            β *= 1.1
-            ϵ *= 1.1
+            movelimit *= 1.1
+            constrRelax *= 1.1
         else
-            β = options.nt.β
-            ϵ = options.nt.ϵ
+            movelimit = options.nt.movelimit
+            constrRelax = options.nt.constrRelax
             # Store recent history of objectives
-            comps[1:end-1] .= comps[2:end]
+            objHist[1:end-1] .= objHist[2:end]
             objval, objgrad = NonconvexCore.value_gradient(getobjective(model), x)
-            comps[end] = objval
-            if count > N
-                er = abs(sum([comps[i] - comps[i - 1] for i in 2:N])) / sum(comps)
+            # Apply "time stabilization"
+            if timeStable
+                objgrad = (objgrad + pastGrad)/2
+                pastGrad = copy(objgrad)
             end
-            @info "iter = $count, obj = $(round.(comps[end]; digits=3)), constr_vio_norm = $(round(norm(currentConstr), digits=3)), er = $(round(er, digits=3)) Δ = $(Δ[1])"
+            objHist[end] = objval
+            if count > pastN
+                er = abs(sum([objHist[i] - objHist[i - 1] for i in 2:pastN])) / sum(objHist)
+            end
+            @info "iter = $count, obj = $(round.(objHist[end]; digits=3)), constr_vio_norm = $(round(norm(currentConstr), digits=3)), er = $(round(er, digits=3))"
         end
         count += 1
     end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,45 +1,60 @@
-using NonconvexTOBS, TopOpt, LinearAlgebra, Test
+using NonconvexTOBS, TopOpt, LinearAlgebra, Test, GLMakie
 
 @testset "Example" begin
     E = 1.0 # Young’s modulus
     v = 0.3 # Poisson’s ratio
     f = 1.0 # downward force
-    rmin = 4.0 # filter radius
-    xmin = 0.0001 # minimum density
+    rmin = 6.0 # filter radius
+    xmin = 0.001 # minimum density
     V = 0.5 # maximum volume fraction
-    p = 4.0 # penalty
+    p = 3.0 # penalty
 
-    problem_size = (60, 20)
-    x0 = fill(1.0, prod(problem_size)) # initial design
-    problem = HalfMBB(Val{:Linear}, problem_size, (1.0, 1.0), E, v, f)
-
-    # problem_size = (160, 40)
+    # problem_size = (60, 20)
     # x0 = fill(1.0, prod(problem_size)) # initial design
-    # problem = PointLoadCantilever(Val{:Linear}, problem_size, (1.0, 1.0), E, v, f)
+    # problem = HalfMBB(Val{:Linear}, problem_size, (1.0, 1.0), E, v, f)
+
+    global problem_size = (160, 100)
+    x0 = fill(1.0, prod(problem_size)) # initial design
+    problem = PointLoadCantilever(Val{:Linear}, problem_size, (1.0, 1.0), E, v, f)
 
     solver = FEASolver(Direct, problem; xmin=xmin)
     cheqfilter = DensityFilter(solver; rmin=rmin)
     comp = TopOpt.Compliance(problem, solver)
 
-    function obj(x)
-        # minimize compliance
-        return comp(cheqfilter(x))
-    end
-    function constr(x)
-        # volume fraction constraint
-        return sum(cheqfilter(x)) / length(x) - V
-    end
+    obj(x) = comp(cheqfilter(x)) # compliance objective
+
+    constr(x) = sum(cheqfilter(x)) / length(x) - V # volume fraction constraint
 
     m = Model(obj)
     addvar!(m, zeros(length(x0)), ones(length(x0)))
     Nonconvex.add_ineq_constraint!(m, constr)
 
-    options = TOBSOptions()
+    options = TOBSOptions(;
+        constrRelax = 0.01,
+        movelimit = 1,
+        timeStable = true
+    )
     TopOpt.setpenalty!(solver, p)
     # TOBS
     @time r = Nonconvex.optimize(m, TOBSAlg(), x0; options=options)
 
     @show obj(r.minimizer)
     @show constr(r.minimizer)
-    topology = r.minimizer
+    global topology = r.minimizer
 end
+
+function dispQuad(nelx,nely,vec)
+    # nelx = number of elements along x axis (number of columns in matrix)
+    # nely = number of elements along y axis (number of lines in matrix)
+    # vec = vector of scalars, each one associated to an element.
+      # this vector is already ordered according to element IDs
+    quadVec=zeros(nely,nelx)
+    for i in 1:nely
+      for j in 1:nelx
+        quadVec[nely-(i-1),j] = vec[(i-1)*nelx+1+(j-1)]
+      end
+    end
+    display(heatmap(1:nelx,1:nely,quadVec'))
+end
+
+dispQuad(problem_size...,topology)
